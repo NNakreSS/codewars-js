0ccf6febc4d8cfdad2191f119ce46c42807e71ef
NNakreSS <87872407+NNakreSS@users.noreply.github.com>
2023-09-04 00:48:05 +0300

    first commit

---
+++ high-low.js
@@ -0,0 +1,40 @@
+
+// solution 1
+function highAndLow(numbers) {
+    const numbersArray = getNumbersToString(numbers)
+    console.log(numbersArray)
+    const high = Math.max(...numbersArray)
+    const low = Math.min(...numbersArray)
+    return (high + " " + low)
+}
+
+function getNumbersToString(stringNumbers) {
+    const stNumbers = stringNumbers.split(" ")
+    let numbers = []
+    for (let i = 0; i < stNumbers.length; i++) {
+        const int = Number(stNumbers[i])
+        if (!isNaN(int)) numbers.push(int);
+    }
+    return numbers
+}
+
+function start() {
+    console.log(highAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4"))
+    console.log(highAndLow("1 2 3"))
+}
+
+start();
+
+// solution 2 
+
+function highAndLow2(numbers) {
+    const numbersArray = numbers.split(" ")
+    return (`${Math.max(...numbersArray)} ${Math.min(...numbersArray)}`)
+}
+
+function start2() {
+    console.log(highAndLow2("8 3 -5 42 -1 0 0 -9 4 7 4 -4"))
+    console.log(highAndLow2("1 2 3"))
+}
+
+start2();
